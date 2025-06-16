c8e7c34c04d633a2fa7a0c91173ea2511b7edff9
dean <dean@beamdot3d.com>
2025-06-16 10:20:00 -0600

    console logs added

--- dist/script.js
+++ dist/script.js
@@ -1,5 +1,2 @@
-(() => {
-  // script.js
-  console.log("hi world");
-})();
+(()=>{var n=document.querySelector(".action-btn.location"),o=document.querySelector(".action-btn.rotation"),t=document.querySelector(".action-btn.scale");n.addEventListener("click",function(){console.log("location!")});o.addEventListener("click",function(){console.log("rotation!")});t.addEventListener("click",function(){console.log("scale!")});})();
 //# sourceMappingURL=script.js.map
--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("hi world");
+const locationBtn = document.querySelector(".action-btn.location");
+const rotationBtn = document.querySelector(".action-btn.rotation");
+const scaleBtn = document.querySelector(".action-btn.scale");
+
+locationBtn.addEventListener("click", function () {
+  console.log("location!");
+});
+rotationBtn.addEventListener("click", function () {
+  console.log("rotation!");
+});
+scaleBtn.addEventListener("click", function () {
+  console.log("scale!");
+});
