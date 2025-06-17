c94838f984aea5393cc2d05efdea3b322429be64
dean <dean@beamdot3d.com>
2025-06-17 08:11:45 -0600

    buttons active

--- dist/script.js
+++ dist/script.js
@@ -1,15 +1,49 @@
 (() => {
   // script.js
+  var vidWrapper = document.querySelector(".vid-wrapper");
+  var locationVid = document.getElementById("locationVid");
+  var rotationVid = document.getElementById("rotationVid");
+  var scaleVid = document.getElementById("scaleVid");
+  var allVids = document.querySelectorAll(".vid");
   var locationBtn = document.querySelector(".action-btn.location");
   var rotationBtn = document.querySelector(".action-btn.rotation");
   var scaleBtn = document.querySelector(".action-btn.scale");
+  var activeVideo;
+  var isPlaying = false;
+  vidWrapper.addEventListener("click", function() {
+    PausePlayVid();
+  });
   locationBtn.addEventListener("click", function() {
-    console.log("location!");
+    SetActiveVid(locationVid);
   });
   rotationBtn.addEventListener("click", function() {
-    console.log("rotation!");
+    SetActiveVid(rotationVid);
   });
   scaleBtn.addEventListener("click", function() {
-    console.log("scale clicked!");
+    SetActiveVid(scaleVid);
   });
+  var SetActiveVid = function(activeVid) {
+    activeVideo = activeVid;
+    allVids.forEach((el) => {
+      el.pause();
+      el.currentTime = 0;
+      el.style.display = "none";
+      if (el.id === activeVid.id) {
+        el.style.display = "block";
+      }
+      activeVid.play();
+      isPlaying = true;
+    });
+  };
+  var PausePlayVid = function() {
+    if (activeVideo) {
+      if (isPlaying) {
+        activeVideo.pause();
+        isPlaying = false;
+      } else {
+        activeVideo.play();
+        isPlaying = true;
+      }
+    }
+  };
 })();
--- script.js
+++ script.js
@@ -1,13 +1,51 @@
+const vidWrapper = document.querySelector(".vid-wrapper");
+const locationVid = document.getElementById("locationVid");
+const rotationVid = document.getElementById("rotationVid");
+const scaleVid = document.getElementById("scaleVid");
+const allVids = document.querySelectorAll(".vid");
 const locationBtn = document.querySelector(".action-btn.location");
 const rotationBtn = document.querySelector(".action-btn.rotation");
 const scaleBtn = document.querySelector(".action-btn.scale");
 
+let activeVideo;
+let isPlaying = false;
+
+vidWrapper.addEventListener("click", function () {
+  PausePlayVid();
+});
+
 locationBtn.addEventListener("click", function () {
-  console.log("location!");
+  SetActiveVid(locationVid);
 });
 rotationBtn.addEventListener("click", function () {
-  console.log("rotation!");
+  SetActiveVid(rotationVid);
 });
 scaleBtn.addEventListener("click", function () {
-  console.log("scale clicked!");
+  SetActiveVid(scaleVid);
 });
+
+const SetActiveVid = function (activeVid) {
+  activeVideo = activeVid;
+  allVids.forEach((el) => {
+    el.pause();
+    el.currentTime = 0;
+    el.style.display = "none";
+    if (el.id === activeVid.id) {
+      el.style.display = "block";
+    }
+    activeVid.play();
+    isPlaying = true;
+  });
+};
+
+const PausePlayVid = function () {
+  if (activeVideo) {
+    if (isPlaying) {
+      activeVideo.pause();
+      isPlaying = false;
+    } else {
+      activeVideo.play();
+      isPlaying = true;
+    }
+  }
+};
